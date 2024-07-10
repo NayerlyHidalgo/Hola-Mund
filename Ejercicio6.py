7b7b7cdd88f13b6a69bf30af3c9008e1f152daf9
NayerlyHidalgo <nayerly.hidalgo@ute.edu.ec>
2024-07-09 19:38:06 -0500

    Ejercicio6

---
+++ Ejercicio6.py
@@ -0,0 +1,6 @@
+Cálculo del área de un círculo: Escribe un programa que calcule el área de un círculo a partir de su radio, ingresado por el usuario.
+
+radio = int(input(" Ingrese el radio del circulo "))#Solicitamos el radio del circulo
+
+area = 3.141592653589793 * (radio * radio) #Usamos la formula que es Pi2radio(3.11415...) mutiplicado por 2 veces el radio solicitado
+print("El area del circulo es:", area, "centimetros cuadrados.")
