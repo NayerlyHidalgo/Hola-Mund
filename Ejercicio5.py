a0c94f0a61f0c220732dec71921d82558d274146
NayerlyHidalgo <nayerly.hidalgo@ute.edu.ec>
2024-07-09 19:36:53 -0500

    Ejercicio5

---
+++ Ejercicio5.py
@@ -0,0 +1,5 @@
+Número mayor: Escribe un programa que pida tres números al usuario y determine cuál es el mayor.
+
+numero1 = int(input(" Ingrese numero "))
+numero2 = int(input(" Ingrese numero "))
+numero3 = int(input(" Ingrese numero "))
